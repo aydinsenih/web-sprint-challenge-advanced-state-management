7e92c99d6a7f1ffe94dcc9ea7585731838abb753
aydinsenih <aydinsenih@gmail.com>
2021-01-22 10:05:07 -0800

    form validation fix

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -53,7 +53,7 @@ class AddForm extends React.Component {
                             name="position"
                             id="position"
                         />
-                        <label htmlFor="nickname">Nick:</label>
+                        <label htmlFor="nickname">Nickname:</label>
                         <br />
                         <input
                             onChange={this.handleChange}
